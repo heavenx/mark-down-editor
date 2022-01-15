50100bda5cd9bc97933effac505dee8ff6325fb4
Abderrahmane Alaoui Mdaghri <alaouiabdou@gmail.com>
2022-01-15 17:16:20 +0100

    all test passed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { marked } from "marked";
+import React from "react";
+import defaultReadMe from './READ.md'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { content: '' };
+    this.handleChange = this.handleChange.bind(this)
+    marked.setOptions({
+      breaks: true,
+      sanitize: true,
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      content: e.target.value
+    })
+  }
+
+  getMarkdownText() {
+    var rawMarkup = marked.parse(this.state.content);
+    return { __html: rawMarkup };
+  }
+  componentWillMount() {
+    fetch(defaultReadMe).then((response) => response.text()).then((text) => {
+      this.setState({ content: text })
+    })
+  }
+
+  render() {
+
+    return (
+      <div className='container' >
+        <textarea value={this.state.content} id="editor" type="textarea" onChange={this.handleChange} />
+        <div id="preview" dangerouslySetInnerHTML={this.getMarkdownText()} />
+      </div>
+    );
+
+  }
 }
 
 export default App;
